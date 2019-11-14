7e79aa5ecd877dcd7dea169872becca1ec87840c
flummgingbird <wwconlin@gmail.com>
2019-11-14 14:50:32 -0600

    use input filename for output file

--- merge_csv.py
+++ merge_csv.py
@@ -4,6 +4,10 @@ import sys
 headers = pd.read_csv('headers.csv')
 input = pd.read_csv(sys.argv[1])
 
+output_filename =  sys.argv[1].split('/')
+
+output_filename = output_filename[-1]
+
 output = headers.append(input, sort=False)
 
-output.to_csv('output.csv', index=False)
+output.to_csv(output_filename, index=False)
